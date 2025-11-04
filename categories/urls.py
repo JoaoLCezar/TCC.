203c8f5c35ad9acc03e601b68038a64eee6d5ae8
João Cezar <joaovictor2010lima@gmail.com>
2025-11-03 23:26:29 -0300

    feat(categories): Implementa CRUD completo de Categorias
    Finaliza o módulo de 'categories' com a implementação completa do CRUD (Create, Read, Update, Delete).
    
    Este commit inclui: - **Create:** View 'criar_categoria' e template 'form_categoria.html'. - **Read:** View 'listar_categorias' e template 'lista_categorias.html'. - **Update:** View 'atualizar_categoria' (reutilizando o 'form_categoria.html') com a lógica de 'instance'. - **Delete:** View 'excluir_categoria' com o template de confirmação 'categoria_confirm_delete.html'.
    
    Todas as views foram corrigidas (imports, NameErrors, AttributeErrors, Typos) e estão funcionando corretamente, lendo e escrevendo no banco de dados PostgreSQL.

--- categories/urls.py
+++ categories/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.listar_categorias, name='listar_categorias'),
     
     path('nova/', views.criar_categoria, name='criar_categoria'),
+
+    path('<int:pk>/editar/', views.atualizar_categoria, name='atualizar_categoria'),
     
+    path('<int:pk>/excluir/', views.excluir_categoria, name='excluir_categoria'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404,get_list_or_404
 from .models import Categoria
+from .forms import CategoriaForm
 
 def listar_categorias(request):
     categorias = Categoria.objects.all()
@@ -27,4 +28,36 @@ def criar_categoria(request):
     }
     return render(request, 'categories/form_categoria.html', context)
 
+def atualizar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk = pk)
+
+    if request.method =='POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('categories:listar_categorias')
+        
+    else:
+        form = CategoriaForm(instance=categoria)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'categories/form_categoria.html', context)
+
+
+def excluir_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk = pk)
+
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('categories:listar_categorias') 
+
+    context = {
+        'categoria': categoria
+    }   
+
+    return render(request, 'categories/categoria_confirm_delete.html', context)
 
